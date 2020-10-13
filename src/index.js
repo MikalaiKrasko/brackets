1b9be7716aa219ded017d97bcda9ab26b669d63d
MikalaiKrasko <mikalai.krasko@gmail.com>
2020-10-13 20:21:25 +0300

    init: initial task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let leftArr=[];
+    let rightArr = [];
+    for(let i=0; i<bracketsConfig.length; i++){
+      if(bracketsConfig[i] === '(' || bracketsConfig[i] === '[' || bracketsConfig[i] === "{"){
+         leftArr.push(bracketsConfig[i]);
+        }
+        
+        
+        if(bracketsConfig[i] === ')'){
+      
+          rightArr.push("(");
+        }else if(bracketsConfig[i] === '}'){
+        
+          rightArr.push("{");
+        } else if(bracketsConfig[i] === ']'){
+        
+          rightArr.push("[");
+        }
+   }
+  
+   rightArr.reverse();
+    
+    if(leftArr.length<rightArr.length || leftArr.length>rightArr.length){
+    return false;
+    }
+    
+    for(let k=0; k<leftArr.length; k++) {
+      if(leftArr[k] != rightArr[k]){
+          return false;
+        }
+    }
+
+    return true;
+    }
+
